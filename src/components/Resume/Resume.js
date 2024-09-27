d3faaa2d00864952ee1abf104a3e52c76974b0c3
malevin <malevin.git@gmail.com>
2024-09-27 23:50:19 +0330

    Adding section skill

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -5,17 +5,18 @@ import { FaGithub, FaTelegramPlane } from 'react-icons/fa';
 
 import './Resume.css';
 function Resume() {
-  const { t,i18n } = useTranslation();
-  const isRTL = i18n.language === 'fa'; 
+  const { t, i18n } = useTranslation();
+  const isRTL = i18n.language === 'fa';
+  const skills = ['python', 'django', 'flask', 'redis', 'SQL', 'react', 'tkinter'];
   return (
-    <div className='main-resume'>
+    <div className='resume-container'>
       <div className={`about-me-container ${isRTL ? 'rtl' : 'ltr'}`}>
         <h1>{t('about_me.title')}</h1>
         <p >
-       {t('about_me.summary')}
+          {t('about_me.summary')}
         </p>
       </div>
-      <div className="social-media-container">
+      <div className="resume-container">
         <h1>{t('social_media.title')}</h1>
         <div className="social-icons">
           <a href="https://github.com/mohsenakbari79" target="_blank" rel="noopener noreferrer">
@@ -24,6 +25,16 @@ function Resume() {
           <a href="https://t.me/malevin" target="_blank" rel="noopener noreferrer">
             <FaTelegramPlane />
           </a>
+        </div>
+      </div>
+      <div className="resume-container">
+        <h1>{t('skills.title')}</h1>
+        <div className="skills-container">
+          {skills.map((skill, index) => (
+            <div key={index} className="skill-box">
+              {skill}
+            </div>
+          ))}
         </div>
       </div>
 
