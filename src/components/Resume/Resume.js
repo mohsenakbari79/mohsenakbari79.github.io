a545a73dafdcab37cca28f6d48330b430743ac90
malevin <malevin.git@gmail.com>
2024-09-27 22:26:23 +0330

    Adding section about me and social networks to resume section and general corrections

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,13 +1,32 @@
 import React from 'react';
 
 import { useTranslation } from 'react-i18next';
+import { FaGithub, FaTelegramPlane } from 'react-icons/fa';
 
+import './Resume.css';
 function Resume() {
-  const { t } = useTranslation();
-
+  const { t,i18n } = useTranslation();
+  const isRTL = i18n.language === 'fa'; 
   return (
-    <div>
-      resume
+    <div className='main-resume'>
+      <div className={`about-me-container ${isRTL ? 'rtl' : 'ltr'}`}>
+        <h1>{t('about_me.title')}</h1>
+        <p >
+       {t('about_me.summary')}
+        </p>
+      </div>
+      <div className="social-media-container">
+        <h1>{t('social_media.title')}</h1>
+        <div className="social-icons">
+          <a href="https://github.com/mohsenakbari79" target="_blank" rel="noopener noreferrer">
+            <FaGithub />
+          </a>
+          <a href="https://t.me/malevin" target="_blank" rel="noopener noreferrer">
+            <FaTelegramPlane />
+          </a>
+        </div>
+      </div>
+
     </div>
   );
 }
