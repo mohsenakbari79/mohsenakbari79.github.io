d5ed0973dd6b8856e2e1868e5958d9b9a80bc8d1
malevin <malevin.git@gmail.com>
2025-01-16 23:43:26 +0330

    add save language selected to storage for not remove after refresh

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ThemeProvider } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -13,17 +13,30 @@ import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 function App() {
   const { i18n } = useTranslation();
   const [direction, setDirection] = useState('rtl');
+  const [activeLang, setActiveLang] = useState('fa'); // اضافه کردن زبان فعال
+
+  useEffect(() => {
+    const savedLanguage = localStorage.getItem('language') || 'fa';
+    i18n.changeLanguage(savedLanguage);
+    setDirection(savedLanguage === 'fa' ? 'rtl' : 'ltr');
+    setActiveLang(savedLanguage); // تنظیم زبان فعال از localStorage
+  }, [i18n]);
 
   const changeLanguage = (lang) => {
     i18n.changeLanguage(lang);
     setDirection(lang === 'fa' ? 'rtl' : 'ltr');
+    localStorage.setItem('language', lang);
+    setActiveLang(lang); // به‌روزرسانی زبان فعال
   };
+
+
+
 
   return (
     <div className="App">
       <ThemeProvider dir={direction}>
         <Router>
-          <Header changeLanguage={changeLanguage} />
+          <Header changeLanguage={changeLanguage} activeLang={activeLang} />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/contact" element={<Contact />} />
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,37 +1,34 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Navbar, Nav, Button } from 'react-bootstrap';
 import './Header.css';
 import { Link } from 'react-router-dom';
 import { FaHome } from 'react-icons/fa';
-import { GiNotebook } from "react-icons/gi";
+import { GiNotebook } from 'react-icons/gi';
 
-function Header({ changeLanguage }) {
-  const [activeLang, setActiveLang] = useState('fa');
-
-  const handleLanguageChange = (lang) => {
-    setActiveLang(lang);
-    changeLanguage(lang);
-  };
-
+function Header({ changeLanguage, activeLang }) {
   return (
     <Navbar bg="light" className="justify-content-between">
       <div className="language-buttons mr-auto">
         <Button
           className={activeLang === 'fa' ? 'active' : ''}
-          onClick={() => handleLanguageChange('fa')}
+          onClick={() => changeLanguage('fa')}
         >
           فارسی
         </Button>
         <Button
           className={activeLang === 'en' ? 'active' : ''}
-          onClick={() => handleLanguageChange('en')}
+          onClick={() => changeLanguage('en')}
         >
           English
         </Button>
       </div>
       <Nav className="ml-auto">
-      <Nav.Link as={Link} to="/contact" className='space-icon'><GiNotebook /></Nav.Link>
-      <Nav.Link as={Link} to="/" className='space-icon'><FaHome /></Nav.Link>
+        <Nav.Link as={Link} to="/contact" className="space-icon">
+          <GiNotebook />
+        </Nav.Link>
+        <Nav.Link as={Link} to="/" className="space-icon">
+          <FaHome />
+        </Nav.Link>
       </Nav>
     </Navbar>
   );
